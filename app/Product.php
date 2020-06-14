67472ea83e6fecfca68f59e22f8e36fc8073b123
Stefany Livaque <emylivaque@gmail.com>
2020-06-13 20:46:05 -0500

    Configuracion de los modelos

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+
+	const PRODUCT_DISPONIBLE = 'disponible';
+	const PRODUCT_NO_DISPONIBLE = 'no disponible';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id' // Pertenece a => belongs to
+    ];
+    
+    public function estaDisponible()
+    {
+    	return $this->status == Product::PRODUCT_DISPONIBLE;
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Category');
+    }
+    
+    public function seller()
+    {
+        return $this->belongsTo('App\Seller');
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany('App\Transaction');
+    }
 }
--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'quantity',
+        'buyer_id',
+        'product_id'
+    ];
+
+    public function buyer()
+    {
+        return $this->belongsTo('App\Buyer');
+    }
+    
+    public function product()
+    {
+        return $this->belongsTo('App\Product');
+    }
 }
